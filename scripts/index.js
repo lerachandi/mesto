c757028d6c4853f4a5295a9467f5dcb6d6913950
valeriya leonova <lerachandi@yandex.ru>
2023-08-12 18:03:31 +0300

    sprint5

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,143 @@
-// Popup
-const popupElement = document.querySelector(".popup");
-// Кнпока редактирования личного профиля
-const editProfileButton = document.querySelector(".profile__edit-button");
+// Попапы
+const popupElement = document.querySelectorAll(".popup");
+const popupEditProfile = document.querySelector(".popup_edit-profile");
+const popupMesto = document.querySelector(".popup_add-mesto"); // попап добавления нового места
 
-// Кнопка закрытия редактирования личного профиля
-const closeProfileButton = document.querySelector(".popup__close-button");
+//Попап открытия фотокарточки
+const cardPopupImage = document.querySelector(".popup_opened-image"); // попап открытия карточки
+const modalImage = cardPopupImage.querySelector(".popup__image"); // попап открытия карточки места
 
-// Находим форму в DOM
-let formElement = document.querySelector(".popup__form");
+// Профиль: элемент профиля и отображаемые имя с описанием
+const profile = document.querySelector(".profile"); // элемент профиля
+const profileDescription = profile.querySelector(".profile__description"); //отображаемое описание
+const profileName = profile.querySelector(".profile__name"); //отображаемое имя
+const editProfileButton = profile.querySelector(".profile__edit-button"); // кнопка редактирования профиля
 
-// Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__input_profile_edit-name");
-let descriptionInput = document.querySelector(
-  ".popup__input_profile_edit-description"
-);
+//Профиль: форма
+const profileFormElement = popupEditProfile.querySelector(".popup__form"); // форма: название, описание, кнопка
+const nameInput = profileFormElement.querySelector(".popup__input_profile_edit-name"); // инпут имени
+const descriptionInput = profileFormElement.querySelector(".popup__input_profile_edit-description"); // инпут описание
+const profileSubmitButton = popupEditProfile.querySelector(".popup__button-save"); // кнопку отправки
 
-// Клик по карандашу для открытия попапа
-editProfileButton.addEventListener("click", openPopup);
+//Место: для загрузки картинок
+const cards = document.querySelector(".cards");
+const cardsTemplate = document.querySelector("#cards-template").content;
 
-// Клик по крестику для закрытия попапа
-closeProfileButton.addEventListener("click", closePopup);
+//Место: форма
+const addFormElement = popupMesto.querySelector(".popup__form");
+const cardNameInput = popupMesto.querySelector(".popup__input_mesto-name"); // инпут названия
+const cardUrlInput = popupMesto.querySelector(".popup__input_mesto-url"); // инпут ссылки на изображение
+const buttonAddMesto = document.querySelector(".profile__add-button"); // кнопка добавления места
+const formAddMestoSubmitButton = popupMesto.querySelector(".popup__button-save"); // кнопка отправки формы Места "Создать"
+const cardsImage = document.querySelector(".cards__image");
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Кнопка закрытия попапов (для всех)
+const closeButtons = document.querySelectorAll(".popup__close-button");
+
+//Попап открытой карточки места
+const modalTitle = cardPopupImage.querySelector(".popup__description");
+
+//Создание карточек
+function createCards(elements) {
+  // сначала копируем ноду с темплейтом,
+  // затем находим селекторы изображения, названием, удалением и лайка
+  const cards = cardsTemplate.cloneNode(true);
+  const cardsImage = cards.querySelector(".cards__image");
+  const cardsName = cards.querySelector(".cards__title");
+  const cardsDeleteButton = cards.querySelector(".cards__delete");
+  const cardsLikeButton = cards.querySelector(".cards__like");
+
+  cardsName.textContent = elements.name; // подтягиваем название из initialCards
+  cardsImage.src = elements.link; // подтягиваем ссылку из initialCards
+  cardsImage.alt = `Фотография ${elements.name}`; // альтернативный текст
+
+  // слушатель на клик по лайку, после клика класс должен меняться,
+  // т.е. после клика лайк становится черным
+  cardsLikeButton.addEventListener("click", () => {
+    cardsLikeButton.classList.toggle("cards__like_active");
+  });
+
+  // слушатель для клика по корзине
+  cardsDeleteButton.addEventListener("click", () => {
+    const cardsItem = cardsDeleteButton.closest(".cards__item");
+    cardsItem.remove();
+  });
+
+  // слушатель для открытия попапа картинки
+  cardsImage.addEventListener("click", () => {
+    modalImage.src = elements.link;
+    modalImage.alt = `Фотография ${elements.name}`;
+    modalTitle.textContent = elements.name;
+    openPopup(cardPopupImage);
+  });
+
+  return cards;
+}
+
+//добавление карточки в начало списка
+function renderCard(item) {
+  cards.prepend(createCards(item));
+}
+
+//считываем и передаем элементы из массива initialCards
+function cardArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    renderCard(arr[i]);
+  }
+}
+
+//функция создания нового места из формы
+function addMesto(evt) {
+  evt.preventDefault();
+  const createMesto = {};
+  createMesto.name = cardNameInput.value;
+  createMesto.link = cardUrlInput.value;
+  renderCard(createMesto);
+  closePopup(popupMesto);
+}
+
+// Редактирование профиля: обработчик «отправки» формы
 function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Получение значений полей descriptionInput и nameInput из свойства value
+  // Получение значений полей descriptionInput и nameInput из свойства value:
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  closePopup();
+  closePopup(popupEditProfile); // попап закроется по клику на кнопку сохранения
 }
+
+//Открытие попапа
+function openPopup(evt) {
+  evt.classList.add("popup_opened");
+}
+
+//Закрытие попапов
+function closePopup(evt) {
+  evt.classList.remove("popup_opened");
+}
+
+//Создание слушателей для кнопок закрытия
+closeButtons.forEach((button) => {
+  const popup = button.closest(".popup");
+  button.addEventListener("click", () => closePopup(popup));
+});
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", handleFormSubmit);
+profileFormElement.addEventListener("submit", handleFormSubmit);
+addFormElement.addEventListener("submit", addMesto);
 
-let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
+buttonAddMesto.addEventListener("click", () => {
+  addFormElement.reset(); // Очищение полей при открытии, т.е.
+  //без этого в форме могут отображаться старые значения, если они не были отправлены
+  openPopup(popupMesto);
+});
 
-//Открытие попапа
-function openPopup() {
-  popupElement.classList.add("popup_opened");
-  //Берем значения из профиля и  кладем в инпуты
+//Слушатель для редактирования профиля
+editProfileButton.addEventListener("click", () => {
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
-}
+  openPopup(popupEditProfile);
+});
 
-//Закрытие попапа
-function closePopup() {
-  popupElement.classList.remove("popup_opened");
-}
+//карточки из initialCards
+cardArray(initialCards);
---
+++ scripts/initialCards.js
@@ -0,0 +1,31 @@
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Северная Осетия',
+    link: 'https://images.unsplash.com/photo-1658911005996-26d19a83a10f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80'
+  },
+  {
+    name: 'Эстосадок',
+    link: 'https://images.unsplash.com/photo-1595954011025-b4a68c838562?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1626&q=80'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Бишкек',
+    link: 'https://images.unsplash.com/photo-1671124918982-5b038668cbd1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+];
